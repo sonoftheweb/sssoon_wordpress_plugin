33eb1c5521a648f2a09f42e507cb221a16ca3b55
sonoftheweb <prometheus6001@gmail.com>
2015-08-28 18:49:12 +0100

    Initial Comit

---
+++ lib/sssoon-front-view.php
@@ -0,0 +1,165 @@
+<?php
+
+
+function sssoon_front_view()
+{
+
+//require_once ('sssoon-front-view.php' );
+    $content = get_option('sssoon_options');
+    $settings = get_option('sssoon_design_options');
+    $general = get_option('sssoon_options_settings'); ?>
+
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <meta name="viewport" content="width=device-width" />
+
+        <title><?php echo get_bloginfo('name'); ?> - <?php echo $content['title']; ?></title>
+
+        <link rel="stylesheet" href="<?php echo plugins_url('lib/css/coming-sssoon.css', dirname(__FILE__)); ?>">
+        <link rel="stylesheet" href="<?php echo plugins_url('lib/css/bootstrap.css', dirname(__FILE__)); ?>">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+        <?php
+        $design_optios = get_option('sssoon_design_options');
+        echo '<style>';
+
+
+        if ($design_optios['bg'] == 'color'):
+            echo '.main{
+                    background:' . $design_optios['customcolor'].';
+                 }';
+        elseif($design_optios['bg'] == 'image'):
+            echo '.main{
+                    background:url(' . $design_optios['bgimg']. ') no-repeat;
+                  }';
+        endif;
+
+        echo '</style>';
+
+
+        ?>
+
+
+    </head>
+    <body>
+
+    <nav class="navbar navbar-transparent navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <img src="images/flags/US.png"/>
+                            English(US)
+                            <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#"><img src="images/flags/DE.png"/> Deutsch</a></li>
+                            <li><a href="#"><img src="images/flags/GB.png"/> English(UK)</a></li>
+                            <li><a href="#"><img src="images/flags/FR.png"/> Français</a></li>
+                            <li><a href="#"><img src="images/flags/RO.png"/> Română</a></li>
+                            <li><a href="#"><img src="images/flags/IT.png"/> Italiano</a></li>
+
+                            <li class="divider"></li>
+                            <li><a href="#"><img src="images/flags/ES.png"/> Español <span class="label label-default">soon</span></a></li>
+                            <li><a href="#"><img src="images/flags/BR.png"/> Português <span class="label label-default">soon</span></a></li>
+                            <li><a href="#"><img src="images/flags/JP.png"/> 日本語 <span class="label label-default">soon</span></a></li>
+                            <li><a href="#"><img src="images/flags/TR.png"/> Türkçe <span class="label label-default">soon</span></a></li>
+
+                        </ul>
+                    </li>
+
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-facebook-square"></i>
+                            Share
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-twitter"></i>
+                            Tweet
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-envelope-o"></i>
+                            Email
+                        </a>
+                    </li>
+                </ul>
+
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container -->
+    </nav>
+    <div class="main" <!--style="background-image: url('images/default.jpg')-->">
+
+        <!--    Change the image source '/images/default.jpg' with your favourite image.     -->
+
+        <?php if ($design_optios['bg'] == 'image'): ?>
+            <div class="cover black" data-color="black"></div>
+        <?php endif; ?>
+
+        <!--   You can change the black color for the filter with those colors: blue, green, red, orange       -->
+
+        <div class="container">
+            <h1 class="logo cursive">
+                <?php echo $content['heading']; ?>
+            </h1>
+            <!--  H1 can have 2 designs: "logo" and "logo cursive"           -->
+
+            <div class="content">
+                <h4 class="motto"><?php echo $content['abouttext']; ?></h4>
+                <div class="subscribe">
+                    <h5 class="info-text">
+                        Join the waiting list for the beta. We keep you posted.
+                    </h5>
+                    <div class="row">
+                        <div class="col-md-4 col-md-offset-4 col-sm6-6 col-sm-offset-3 ">
+                            <form class="form-inline" role="form">
+                                <div class="form-group">
+                                    <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                    <input type="email" class="form-control transparent" placeholder="Your email here...">
+                                </div>
+                                <button type="submit" class="btn btn-danger btn-fill">Notify Me</button>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="container">
+                <?php if(!empty($general['credits'])):?>
+                Made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com">Creative Tim</a>. Free download <a href="http://www.creative-tim.com/product/coming-sssoon-page">here.</a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+
+    </body>
+    <script src="<?php echo plugins_url('lib/js/jquery-1.10.2.js', dirname(__FILE__)); ?>" type="text/javascript"></script>
+    <script src="<?php echo plugins_url('lib/js/bootstrap.min.js', dirname(__FILE__)); ?>" type="text/javascript"></script>
+    </html>
+<?php }
+
+?>
